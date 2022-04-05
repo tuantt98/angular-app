c01766bf90d1ca3622ed9fdc3197bf3e6f4eadb6
tuantt98 <kisuvitinh@gmail.com>
2022-04-05 21:40:28 +0700

    Add ingredient

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,4 +14,8 @@ export class ShoppingListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onIngredientsAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
