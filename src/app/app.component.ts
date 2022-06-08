6da71d29282cf9b057d9c8742ca0720c77944729
Tuấn Trần <kisuvitinh@gmail.com>
2022-06-08 15:17:51 +0700

    Apply routes (s1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   loadedFeature: string = 'recipe';
-
-  onNavigate(feature: any) {
-    this.loadedFeature = feature;
-  }
 }
